8cb5cf02fbca92634b87a39841657318ae6bfca8
Dogukan Cifci <dogukancifcii5@gmail.com>
2024-09-07 22:58:39 +0300

    Security UserDetailsImpl

---
+++ student-management/src/main/java/com/dogukan/security/service/UserDetailsImpl.java
@@ -0,0 +1,82 @@
+package com.dogukan.security.service;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserDetailsImpl implements UserDetails {
+
+    private Long id;
+
+    private String username;
+
+    private String name;
+
+    private Boolean isAdviser;
+
+    @JsonIgnore
+    private String password;
+
+    private String ssn;
+
+    private Collection<? extends GrantedAuthority> authorities;
+
+    public UserDetailsImpl(Long id, String username, String name, Boolean isAdviser, String password, String role, String ssn) {
+        this.id = id;
+        this.username = username;
+        this.name = name;
+        this.isAdviser = isAdviser;
+        this.password = password;
+        List<GrantedAuthority> grantedAuthorities = new ArrayList<>();
+        grantedAuthorities.add(new SimpleGrantedAuthority(role));
+        //yukarida rol olarak security GrantedAuthority turunde istedigi icin bu donusumu yapmak zorunda kaldik
+        this.authorities = grantedAuthorities;
+        this.ssn = ssn;
+    }
+
+
+    @Override
+
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
