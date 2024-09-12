906454d95114aeee406d4ccd21c4ad780e4492ad
Dogukan Cifci <dogukancifcii5@gmail.com>
2024-09-12 23:40:40 +0300

    JwtUtils

--- student-management/src/main/java/com/dogukan/security/jwt/JwtUtils.java
+++ student-management/src/main/java/com/dogukan/security/jwt/JwtUtils.java
@@ -15,6 +15,7 @@ import java.util.Date;
 //@Slf4j //bu anatasyon assagidaki isi yapiyor aslinda.Loglama isine yariyor.
 public class JwtUtils {
 
+    //gelen hatalri burada log olarak kayit ettik.
     private static final Logger LOGGER = LoggerFactory.getLogger(JwtUtils.class);
 
     @Value("${backendapi.app.jwtExpirationMs}")
@@ -25,27 +26,34 @@ public class JwtUtils {
 
     //TODO: Generate JWT *******************************
 
+    //Burada kullanici sisteme login olmali ve authentication yapmali
+    //login olan kullaniciyi security contexte atiyoduk.Assagida o yuzden Authentication yazdik.Authentication nesnesi sayesinde login olan kullanicinin bilgisini alabilicez.
+    //
     public String generateJwtToken(Authentication authentication) {
+        //Burada anlik olan giris yapmis olan kullaniciyi bana getPrincipal methodu ile getiriyor.UserDetailImpl ile benim user nesneme ceviriyorum.
         UserDetailsImpl userDetails = (UserDetailsImpl) authentication.getPrincipal();
 
+        //unique bir bilgi olan usernameyi jwt token icerisine koyduk.
+        //sebebi kime ait oldugunu bulmak icin unique bilgi koyduk.
         return generateTokenFromUsername(userDetails.getUsername());
         //username kullanarak jwt token create ettik
     }
 
     private String generateTokenFromUsername(String username) {
         return Jwts.builder()
-                .setSubject(username)
-                .setIssuedAt(new Date())
-                .setExpiration(new Date(new Date().getTime() + jwtExpirationMs))
-                .signWith(SignatureAlgorithm.HS512, jwtSecret)
-                .compact();
+                .setSubject(username) //tokene username ekleme(token kime ait)
+                .setIssuedAt(new Date()) //olusturma tarihi
+                .setExpiration(new Date(new Date().getTime() + jwtExpirationMs))//token'ın ne zaman sona ereceğini belirler.
+                .signWith(SignatureAlgorithm.HS512, jwtSecret)//token’ı bir imza algoritması kullanarak imzalar. Bu imza, token'ın bütünlüğünü doğrulamak için gereklidir.
+                .compact(); //JWT'nin tüm bu bilgileri birleştirerek nihai token'ı oluşturur ve string olarak geri döndürür.
     }
 
     //TODO: Validate JWT *******************************
-
+    //jwt token kontrolu yaptigimiz yer.
     public boolean validateJwtToken(String jwtToken) {
 
         try {
+            //parser jwt tokeni belli parcalara boler
             Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(jwtToken);
             return true;
         } catch (ExpiredJwtException e) {
@@ -63,12 +71,13 @@ public class JwtUtils {
     }
 
     //TODO: getUsernameFromJWT *************************
+    //tokenden username getiren yardimci method.
     public String getUsernameFromJwtToken(String token) {
         return Jwts.parser()
                 .setSigningKey(jwtSecret)
                 .parseClaimsJws(token)
-                .getBody()
-                .getSubject();
+                .getBody()//subject bodyde bulunur
+                .getSubject();//subjecti yani user name getirmis olduk.
     }
 
 }
