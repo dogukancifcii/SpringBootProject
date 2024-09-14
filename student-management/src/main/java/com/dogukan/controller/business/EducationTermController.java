09a003daaec5da9d41a4c74059d2831a8c7b3cf9
Dogukan Cifci <dogukancifcii5@gmail.com>
2024-09-14 23:19:37 +0300

    getAllEducationTermsByPage

--- student-management/src/main/java/com/dogukan/controller/business/EducationTermController.java
+++ student-management/src/main/java/com/dogukan/controller/business/EducationTermController.java
@@ -4,11 +4,11 @@ package com.dogukan.controller.business;
 import com.dogukan.payload.response.business.EducationTermResponse;
 import com.dogukan.service.business.EducationTermService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/educationTerm")
@@ -21,7 +21,25 @@ public class EducationTermController {
 
     @GetMapping("/{id}") // http://localhost:8080/educationTerm/1 + GET
     @PreAuthorize("hasAnyAuthority('ADMIN','MANAGER','ASSISTANT_MANAGER','TEACHER')")
-    public EducationTermResponse getEducationTermById(@PathVariable Long id){
+    public EducationTermResponse getEducationTermById(@PathVariable Long id) {
         return educationTermService.getEducationTermResponseById(id);
+    }
+
+
+    @GetMapping("/getAll")// http://localhost:8080/educationTerm/getAll + GET
+    @PreAuthorize("hasAnyAuthority('ADMIN','MANAGER','ASSISTANT_MANAGER','TEACHER')")
+    public List<EducationTermResponse> getAllEducationTerms() {
+        return educationTermService.getAllEducationTerms();
+    }
+
+    @GetMapping("/getAllEducationTermsByPage")// http://localhost:8080/educationTerm/getAll + GET
+    @PreAuthorize("hasAnyAuthority('ADMIN','MANAGER','ASSISTANT_MANAGER','TEACHER')")
+    public Page<EducationTermResponse> getAllEducationTermsByPage(
+            @RequestParam(value = "page", defaultValue = "0") int page,
+            @RequestParam(value = "size", defaultValue = "10") int size,
+            @RequestParam(value = "sort", defaultValue = "startDate") String sort,
+            @RequestParam(value = "type", defaultValue = "desc") String type
+    ) {
+        return educationTermService.getAllEducationTermsByPage(page, size, sort, type);
     }
 }
--- student-management/src/main/java/com/dogukan/service/business/EducationTermService.java
+++ student-management/src/main/java/com/dogukan/service/business/EducationTermService.java
@@ -7,14 +7,21 @@ import com.dogukan.payload.mappers.EducationTermMapper;
 import com.dogukan.payload.messages.ErrorMessages;
 import com.dogukan.payload.response.business.EducationTermResponse;
 import com.dogukan.repository.user.business.EducationTermRepository;
+import com.dogukan.service.helper.PageableHelper;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class EducationTermService {
     private final EducationTermRepository educationTermRepository;
     private final EducationTermMapper educationTermMapper;
+    private final PageableHelper pageableHelper;
 
     public EducationTermResponse getEducationTermResponseById(Long id) {
         EducationTerm educationTerm = isEducationTermExist(id);
@@ -25,5 +32,21 @@ public class EducationTermService {
         return educationTermRepository.findById(id).orElseThrow(
                 () -> new ResourceNotFoundException(String.format(ErrorMessages.EDUCATION_TERM_NOT_FOUND_MESSAGE, id))
         );
+    }
+
+    public List<EducationTermResponse> getAllEducationTerms() {
+        return educationTermRepository.findAll().stream()
+                .map(educationTermMapper::mapEducationTermToEducationTermResponse)
+                .collect(Collectors.toList());
+
+    }
+
+    public Page<EducationTermResponse> getAllEducationTermsByPage(int page, int size, String sort, String type) {
+        Pageable pageable = pageableHelper.getPageableWithProperties(page, size, sort, type);
+
+
+        return educationTermRepository.findAll(pageable)
+                .map(educationTermMapper::mapEducationTermToEducationTermResponse);
+
     }
 }
