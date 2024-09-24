101403e18d9423b435a2130c08c0eb9450b6ac71
Dogukan Cifci <dogukancifcii5@gmail.com>
2024-09-24 18:38:54 +0300

    ContactMessageAuthorize

--- student-management/src/main/java/com/dogukan/contactmessage/controller/ContactMessageController.java
+++ student-management/src/main/java/com/dogukan/contactmessage/controller/ContactMessageController.java
@@ -12,6 +12,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -32,6 +33,7 @@ public class ContactMessageController {
     }
 
     @GetMapping("/getAll") // http://localhost:8080/contactMessages/getAll + GET
+    @PreAuthorize("hasAnyAuthority('ADMIN','MANAGER','ASSISTANT_MANAGER')")
     public Page<ContactMessageResponse> getAll(
             @RequestParam(value = "page", defaultValue = "0") int page,
             @RequestParam(value = "size", defaultValue = "10") int size,
@@ -42,6 +44,7 @@ public class ContactMessageController {
     }
 
     @GetMapping("/searchByEmail")// http://localhost:8080/contactMessages/searchByEmail?email=aaa@bbb.com
+    @PreAuthorize("hasAnyAuthority('ADMIN','MANAGER','ASSISTANT_MANAGER')")
     public Page<ContactMessageResponse> searchByEmail(
             @RequestParam(value = "email") String email,
             @RequestParam(value = "page", defaultValue = "0") int page,
@@ -55,6 +58,7 @@ public class ContactMessageController {
     // Not: *************************************** searchBySubject ***************************************
 
     @GetMapping("/searchBySubject") //http://localhost:8080/contactMessages/searchBySubject?subject=
+    @PreAuthorize("hasAnyAuthority('ADMIN','MANAGER','ASSISTANT_MANAGER')")
     public Page<ContactMessageResponse> searchBySubject(
             @RequestParam(value = "subject") String subject,
             @RequestParam(value = "page", defaultValue = "0") int page,
@@ -83,6 +87,7 @@ public class ContactMessageController {
 
     @GetMapping("/searchBetweenDates")
     // http://localhost:8080/contactMessages/searchBetweenDates?beginDate=2023-09-13&endDate=2023-09-15
+    @PreAuthorize("hasAnyAuthority('ADMIN','MANAGER','ASSISTANT_MANAGER')")
     public ResponseEntity<List<ContactMessage>> searchByDateBetween(
             @RequestParam(value = "beginDate") String beginDateString,
             @RequestParam(value = "endDate") String endDateString) {
@@ -98,6 +103,7 @@ public class ContactMessageController {
     }*/
 
     @DeleteMapping("/deleteByIdParam")  //http://localhost:8080/contactMessages/deleteByIdParam?contactMessageId=1
+    @PreAuthorize("hasAnyAuthority('ADMIN','MANAGER','ASSISTANT_MANAGER')")
     public ResponseEntity<String> deleteById(@RequestParam(value = "contactMessageId") Long contactMessageId) {
         return ResponseEntity.ok(contactMessageService.deleteById(contactMessageId));
     }
@@ -111,6 +117,7 @@ public class ContactMessageController {
     }*/
 
     @DeleteMapping("/deleteById/{contactMessageId}")//http://localhost:8080/contactMessages/deleteById/2
+    @PreAuthorize("hasAnyAuthority('ADMIN','MANAGER','ASSISTANT_MANAGER')")
     public ResponseEntity<String> deleteByIdPath(@PathVariable Long contactMessageId) {
         //return ResponseEntity.ok(contactMessageService.deleteById(contactMessageId));
 
@@ -132,6 +139,7 @@ public class ContactMessageController {
 
 
     @GetMapping("/getByIdParam") //http://localhost:8080/contactMessages/getByIdParam?contactMessageId=1
+    @PreAuthorize("hasAnyAuthority('ADMIN','MANAGER','ASSISTANT_MANAGER')")
     public ResponseEntity<ContactMessage> getById(@RequestParam(value = "contactMessageId") Long contactMessageId) {
         return ResponseEntity.ok(contactMessageService.getContactMessageById(contactMessageId));
     }
@@ -148,6 +156,7 @@ public class ContactMessageController {
 
 
     @GetMapping("/getById/{contactMessageId}")//http://localhost:8080/contactMessages/getById/1
+    @PreAuthorize("hasAnyAuthority('ADMIN','MANAGER','ASSISTANT_MANAGER')")
     public ResponseEntity<ContactMessage> getByIdPath(@PathVariable Long contactMessageId) {
         return ResponseEntity.ok(contactMessageService.getContactMessageById(contactMessageId));
     }
